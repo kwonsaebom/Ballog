b73347b6c3c5bdf585bd1601262dfcbabf6fa87d
kwondoha <ehehgk@icloud.com>
2024-07-21 11:34:07 +0900

    chore: 의존성 설치 등

--- App.js
+++ App.js
@@ -2,24 +2,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { colors } from './src/global';
+import StackNavigator from './src/navigation/StackNavigator';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Ballog</Text>
+    <>
+      <StackNavigator />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f8f8f8',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    color: colors.primary,
   },
 });
