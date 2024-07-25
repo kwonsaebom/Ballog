02d2b1907bdbeffb3338d8268fe0004c5e32759f
봄 <>
2024-07-25 18:38:02 +0900

    Feat: Bar (No Dropdown)

--- src/global.js
+++ src/global.js
@@ -1,7 +1,22 @@
 // global.js
 export const colors = {
-    primary: '#C51E3A',
-    secondory: '#CECECE', 
-    text: '#000000',
-    background: '#FFFFFF'
+  primary: "#C51E3A",
+  secondory: "#CECECE",
+  text: "#000000",
+  background: "#FFFFFF",
+};
+
+export const fonts = {
+  sizes: {
+    tiny: "10px",
+    small: "13px",
+    midium: "14px",
+    big: "20px",
+  },
+  weights: {
+    light: "500",
+    regular: "600",
+    bold: "700",
+    extraBold: "1000",
+  },
 };
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,21 +1,44 @@
-// PostScreen.js
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import styled from "styled-components/native";
+import { colors, fonts } from "../global";
+import { AntDesign } from "@expo/vector-icons";
 
 const PostScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>글쓰기</Text>
-    </View>
+    <Container>
+      <Bar>
+        <CloseButton>
+          <AntDesign name="close" size={24} color="#33363f" />
+        </CloseButton>
+        <PostButton>
+          <ButtonText>등록하기</ButtonText>
+        </PostButton>
+      </Bar>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+const Container = styled.View``;
 
+const Bar = styled.View`
+  flex-direction: row;
+  padding: 8px;
+`;
+const CloseButton = styled.TouchableOpacity``;
+
+const PostButton = styled.TouchableOpacity`
+  border-radius: 26px;
+  background-color: ${colors.primary};
+  border: 1px solid ${colors.primary};
+  width: 77px;
+  height: 30px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ButtonText = styled.Text`
+  font-size: ${fonts.sizes.medium};
+  font-weight: ${fonts.weights.regular};
+  color: white;
+`;
 export default PostScreen;
